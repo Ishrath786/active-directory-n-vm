36034f9e8f19380cf802bff425e893cc8733ad68
ishrathfatimazeba0607@gmail.com <ishrath.fatima.zeba@gmail.com>
2021-12-15 11:58:11 +0530

    first commit

---
+++ Vm Clusters.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "VmCluster-RG123"
+  location = "East US"
+
+
+  tags = {
+      environment = "TF-Test"
+  }
+}
+
+module "windowsservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.rg1.name
+  is_windows_image    = true
+  vm_hostname         = "vmcluster"                         
+  admin_password      = "Vmcluster1234567890"                 
+  vm_os_simple        = "WindowsServer"
+  public_ip_dns       = ["winsimplevmips"] 
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+
+  depends_on = [azurerm_resource_group.rg1]
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.rg1.name
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+
+  depends_on = [azurerm_resource_group.rg1]
+}
+
+output "windows_vm_public_name" {
+  value = module.windowsservers.public_ip_dns_name
+}
+
+output "vm_public_ip" {
+  value = module.windowsservers.public_ip_address
+}
+
+output "vm_private_ips" {
+  value = module.windowsservers.network_interface_private_ip
+}
